e19984bf11080a77518acaf27e29e66e96b6d600
Charmela Ahilan <charmela6@gmail.com>
2024-04-18 11:14:10 -0400

    API to POST/CREATE a New Inventory Item completed

--- controllers/inventory-controller.js
+++ controllers/inventory-controller.js
@@ -9,6 +9,55 @@ const getAll = async (_req, res) => {
   }
 };
 
+const validateRequestBody = (req, res, next) => {
+  const { item_name, description, category, status, quantity } = req.body;
+
+  if (!item_name || !description || !category || !status || !quantity) {
+    return res
+      .status(400)
+      .json({ error: "Missing required properties in the request body" });
+  }
+
+  if (isNaN(quantity)) {
+    return res.status(400).json({ error: "Quantity must be a number" });
+  }
+
+  next();
+};
+
+const createInventory = async (req, res) => {
+  const { warehouse_id, item_name, description, category, status, quantity } =
+    req.body;
+
+  try {
+    const [id] = await knex("inventories").insert({
+      warehouse_id,
+      item_name,
+      description,
+      category,
+      status,
+      quantity,
+    });
+
+    const inventoryItem = {
+      id,
+      warehouse_id,
+      item_name,
+      description,
+      category,
+      status,
+      quantity,
+    };
+
+    res.status(201).json(inventoryItem);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to create inventory item" });
+  }
+};
+
 module.exports = {
   getAll,
+  validateRequestBody,
+  createInventory,
 };
