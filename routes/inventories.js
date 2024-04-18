2b88a22ccdf73d49a7813d25ba6bbeb206d040c6
Charmela Ahilan <charmela6@gmail.com>
2024-04-18 13:50:22 -0400

    Back-End: API to PUT/EDIT an Inventory Item/test and complete

--- routes/inventories.js
+++ routes/inventories.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 
 const inventoryController = require("../controllers/inventory-controller");
 
+// Retrieve all inventory items
 router.route("/").get(inventoryController.getAll);
 
 // Create a new inventory item
@@ -9,6 +10,13 @@ router.post(
   "/",
   inventoryController.validateRequestBody,
   inventoryController.createInventory
+);
+
+// Update a single inventory item
+router.put(
+  "/:id",
+  inventoryController.validateRequestBody,
+  inventoryController.updateInventory
 );
 
 module.exports = router;
