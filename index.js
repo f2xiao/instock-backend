3011cb992db9369930aea6218a0699b9a3e850aa
f2xiao <25191617+f2xiao@users.noreply.github.com>
2024-05-14 16:27:11 -0400

    add cors origin config

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ const cors = require("cors");
 const warehousesRoutes = require("./routes/warehouses");
 const inventoriesRoutes = require("./routes/inventories");
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+  })
+);
 app.use(express.json());
 
 app.use("/api/warehouses", warehousesRoutes);
 app.use("/api/inventories", inventoriesRoutes);
-
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
